12abed47339407a12009c6912e8859f81ff9561c
DanialSaleem14 <danialsaleemdgk1@gmail.com>
2023-07-18 02:48:49 +0500

    Portfolio Website

---
+++ main.js
@@ -0,0 +1,8 @@
+const typed = new Typed('.text', {
+    strings: ['Student of Software Engineering <br> at Air University Islamabad, پاکستان','Website Developer & Designer' , ' C++ & Python developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
